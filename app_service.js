bc4e01ad5996b903fc80df8d50c2bd7e117c1c41
nmazzeo <nahuelms@gmail.com>
2020-02-04 18:02:41 -0300

    Actualizo version con logging

--- app_service.js
+++ app_service.js
@@ -2,19 +2,36 @@ var SwaggerExpress = require('swagger-express-mw');
 const express = require('express');
 
 module.exports = {
-    generateSwaggerApp: generateSwaggerApp
+    configurateSwaggerApps: configurateSwaggerApps
 }
 
-function generateSwaggerApp(config, swaggersFiles) {
+function generateSwaggerApp(config, swaggerApp) {
     const app = express();
 
-    swaggersFiles.forEach(swaggerFile => {
+    swaggerApp.swaggers.forEach(swaggerFile => {
         config.swaggerFile = swaggerFile;
+
         SwaggerExpress.create(config, function (err, swaggerExpress) {
             if (err) { throw err; }
             swaggerExpress.register(app);
+
+            if (config.logPaths) {
+                var jsonData = swaggerExpress.runner.swagger.paths;
+                Object.keys(jsonData).forEach(function (key) {
+                    var toLog = {}
+                    toLog[swaggerApp.basePath + key] = jsonData[key];
+                    console.log(toLog);
+                });
+            }
+
         });
     });
 
     return app;
+}
+
+function configurateSwaggerApps(config, app, swaggerApps) {
+    swaggerApps.forEach(swaggerApp => {
+        app.use(swaggerApp.basePath, generateSwaggerApp(config, swaggerApp))
+    });
 }
